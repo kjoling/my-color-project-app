051307842900d6e80cc7bc98848bf52a230d00d3
keeanj <jolingkw@gmail.com>
2022-04-30 19:07:02 -0700

    Add Select and Material UI

--- src/Palette.js
+++ src/Palette.js
@@ -5,17 +5,33 @@ import Navbar from "./Navbar";
 
 export default function Palette(props) {
   const [level, setLevel] = useState(500);
+  const [format, setFormat] = useState("hex");
   const { colors } = props.palette;
-  const colorBoxes = colors[level].map((color) => (
-    <ColorBox background={color.hex} name={color.name} />
-  ));
+  const colorBoxes = colors[level].map((color) => {
+    return (
+      <ColorBox
+        background={color[format]}
+        name={color.name}
+        id={color.id}
+        key={color.id}
+      />
+    );
+  });
 
   const changeLevel = (newLevel) => {
     setLevel(newLevel);
   };
+
+  const changeFormat = (evt) => {
+    setFormat(evt.target.value);
+  };
   return (
     <div className="Palette">
-      <Navbar changeLevel={changeLevel} level={level} />
+      <Navbar
+        changeLevel={changeLevel}
+        level={level}
+        changeFormat={changeFormat}
+      />
       {/*Navbar goes here*/}
       <div className="Palette-colors">{colorBoxes}</div>
       {/*footer goes here*/}
