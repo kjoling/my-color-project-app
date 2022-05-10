ce3bc800999f03730f74c074a64bbb2c4d1af2cd
keeanj <jolingkw@gmail.com>
2022-05-09 21:43:14 -0700

    add DeleteIcon for DraggableColorBox

--- src/DraggableColorBox.js
+++ src/DraggableColorBox.js
@@ -1,5 +1,6 @@
 import React from "react";
 import { css } from "@emotion/css";
+import DeleteIcon from "@mui/icons-material/Delete";
 
 const styles = {
   root: {
@@ -10,6 +11,27 @@ const styles = {
     position: "relative",
     cursor: "pointer",
     marginBottom: "-3.5px",
+  },
+  boxContent: {
+    position: "absolute",
+    padding: "10px",
+    width: "100%",
+    left: "0px",
+    bottom: "0px",
+    color: "black",
+    letterSpacing: "1px",
+    textTransform: "uppercase",
+    fontSize: "12px",
+    display: "flex",
+    justifyContent: "space-between",
+  },
+  deleteIcon: {
+    color: "rgba(0,0,0,.5)",
+    transition: "all 0.15s ease-in-out",
+    "&:hover": {
+      color: "white",
+      transform: "scale(1.3)",
+    },
   },
 };
 
@@ -22,7 +44,20 @@ function DraggableColorBox(props) {
         background-color: ${color};
       `}
     >
-      {name}
+      <div
+        className={css`
+          ${styles.boxContent}
+        `}
+      >
+        <span>{name}</span>
+        <span
+          className={css`
+            ${styles.deleteIcon}
+          `}
+        >
+          <DeleteIcon />
+        </span>
+      </div>
     </div>
   );
 }
